1f84ecd98ddbce81fc44629a92eeb2faa123b0a0
mint639 <101053166+mint639@users.noreply.github.com>
2022-05-29 18:17:22 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -6,14 +6,14 @@
 
 class Markov {
  private:
-    typedef std::deque<std::tring> prefix;
+    typedef std::deque<std::string> prefix;
     std::map<prefix, std::vector<std::string>> statelab;
     int NPREF = 2;
     int MAXGEN = 1000;
 
  public:
-    Markov(vector<string> allwords, int prfx_count, int gen_count);
-    Markov(map<prefix, vector<string>> Gen, int gen_count);
-    string TextGen();
+    Markov(std::vector<std::string> allwords, int prfx_count, int gen_count);
+    Markov(std::map<prefix, std::vector<std::string>> Gen, int gen_count);
+    std::string TextGen();
 };
 
