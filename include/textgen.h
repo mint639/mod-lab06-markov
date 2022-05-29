df72098678cf55bcb74bbf94375849ab0ed84a20
mint639 <101053166+mint639@users.noreply.github.com>
2022-05-29 04:33:35 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -1,19 +1,18 @@
-INCLUDE_TEXTGEN_H
 #include <deque>
 #include <map>
 #include <string>
 #include <vector>
 
-using namespace ::std;
+using namespace std;
 
 class Markov {
-private:
+private: 
     typedef deque<string> prefix;
     map<prefix, vector<string>> statelab;
     int NPREF = 2;
     int MAXGEN = 1000;
 
-public:
+public: 
     Markov(vector<string> allwords, int prfx_count, int gen_count);
     Markov(map<prefix, vector<string>> Gen, int gen_count);
     string TextGen();
