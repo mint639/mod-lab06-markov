c53f5d5b6a496d75327ca344f6d30dd65c087666
mint639 <101053166+mint639@users.noreply.github.com>
2022-05-29 19:41:48 +0300

    Update textgen.cpp

--- src/textgen.cpp
+++ src/textgen.cpp
@@ -37,7 +37,8 @@ std::string Markov::TextGen() {
         prefix prfx;
         for (int i = 0; i < NPREF; i++)
             prfx.push_back(words.at(i));
-        int random = rand_r(unsigned int*1) % statelab.find(prfx)->second.size();
+        extern int rand_r (unsigned int *__seed) = 1;
+        int random = rand_r(&seed) % statelab.find(prfx)->second.size();
         if (statelab.find(prfx)->second.at(random) == "<Last_Prefix>") {
             for (int i = 0; i < NPREF; i++)
                 output += words.at(i) + ' ';
